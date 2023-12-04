9aaf1ba04b95763c6b548f87c04361e1e61b20c6
Stephen Hines <srhines@gmail.com>
2023-12-03 23:48:28 -0800

    Working version for part 2 of aoc04.

--- aoc04/src/main.rs
+++ aoc04/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::min;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::BufRead;
@@ -14,9 +15,8 @@ fn get_input(filename: &str) -> Vec<String> {
     lines
 }
 
-fn score_cards(lines: &[String]) -> u32 {
-    let mut total_score = 0;
-    //let mut symbols: Vec<Symbol> = Vec::new();
+fn read_cards(lines: &[String]) -> Vec<u32> {
+    let mut card_wins: Vec<u32> = Vec::new();
 
     for line in lines {
         let mut winners = HashSet::new();
@@ -34,18 +34,52 @@ fn score_cards(lines: &[String]) -> u32 {
         for n in our_toks {
             let our_num = n.parse::<u32>().unwrap();
             if winners.contains(&our_num) {
-                if score == 0 {
-                    score = 1;
-                } else {
-                    score *= 2;
-                }
+                score += 1;
             }
         }
-        total_score += score;
+        card_wins.push(score);
     }
+
+    card_wins
+}
+
+fn score_cards(lines: &[String]) -> u32 {
+    let card_wins = read_cards(lines);
+
+    //let total_score = card_wins.iter().filter_map::<u32,_>(|&w| if w == 0 { None } else { Some(2_u32.pow(w - 1))}).sum::<u32>();
+    let total_score = card_wins
+        .iter()
+        .filter(|&&w| w != 0)
+        .map(|&w| 2_u32.pow(w - 1))
+        .sum();
 
     println!("Total Score: {}", total_score);
     total_score
+}
+
+fn copy_cards(lines: &[String]) -> u32 {
+    let card_wins = read_cards(lines);
+    let num_card_games = card_wins.len();
+
+    // Start with 1 of each card.
+    let mut cards: Vec<u32> = Vec::new();
+    for _i in 0..num_card_games {
+        cards.push(1);
+    }
+
+    for i in 0..num_card_games {
+        let wins = card_wins[i] as usize;
+        if wins > 0 {
+            let limit = min(i + wins, num_card_games);
+            for j in i + 1..=limit {
+                cards[j] += cards[i]; // We get n copies of the new cards.
+            }
+        }
+    }
+
+    let total_cards = cards.iter().sum();
+    println!("Total Cards: {}", total_cards);
+    total_cards
 }
 
 #[test]
@@ -60,7 +94,21 @@ fn test_part1() {
     assert_eq!(score, 21558);
 }
 
+#[test]
+fn test_prelim2() {
+    let cards = copy_cards(&get_input("prelim.txt"));
+    assert_eq!(cards, 30);
+}
+
+#[test]
+fn test_part2() {
+    let cards = copy_cards(&get_input("input.txt"));
+    assert_eq!(cards, 10425665);
+}
+
 fn main() {
     score_cards(&get_input("prelim.txt"));
     score_cards(&get_input("input.txt"));
+    copy_cards(&get_input("prelim.txt"));
+    copy_cards(&get_input("input.txt"));
 }
