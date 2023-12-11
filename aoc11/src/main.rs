ee8c3507c6bfb555797c848f60d3ff9bd8ca5e6b
Stephen Hines <srhines@gmail.com>
2023-12-10 22:45:42 -0800

    Working solution for aoc11 part 2.

--- aoc11/src/main.rs
+++ aoc11/src/main.rs
@@ -29,19 +29,19 @@ impl Coord {
 #[derive(Debug)]
 struct Universe {
     galaxies: Vec<Coord>,
-    x_coords: HashSet<usize>,
-    y_coords: HashSet<usize>,
-    x_gaps: Vec<usize>,
-    y_gaps: Vec<usize>,
+    x_gaps: HashSet<usize>,
+    y_gaps: HashSet<usize>,
+    expansion: usize,
 }
 
 impl Universe {
-    fn new(lines: &[String]) -> Self {
+    fn new(lines: &[String], expansion: usize) -> Self {
         let mut galaxies = Vec::new();
         let mut x_coords = HashSet::new();
         let mut y_coords = HashSet::new();
-        let mut x_gaps = Vec::new();
-        let mut y_gaps = Vec::new();
+        // Could also use Vec for these, but I'm assuming we have a large set.
+        let mut x_gaps = HashSet::new();
+        let mut y_gaps = HashSet::new();
 
         for (y, line) in lines.iter().enumerate() {
             for (x, c) in line.char_indices() {
@@ -58,25 +58,28 @@ impl Universe {
 
         for x in 0..x_max {
             if !x_coords.contains(&x) {
-                x_gaps.push(x);
+                x_gaps.insert(x);
             }
         }
 
         for y in 0..y_max {
             if !y_coords.contains(&y) {
-                y_gaps.push(y);
+                y_gaps.insert(y);
             }
         }
 
         Self {
             galaxies,
-            x_coords,
-            y_coords,
             x_gaps,
             y_gaps,
+            expansion,
         }
     }
 
+    // Find shortest path between two coordinates
+    // In the case of these up/down/left/right problems, this is just equal
+    // to the x difference plus the y difference. For this problem, we also
+    // have an expansion penalty for empty rows/cols in the Universe.
     fn distance(&self, c1: &Coord, c2: &Coord) -> usize {
         let mut dist = 0;
         if c1 == c2 {
@@ -88,19 +91,19 @@ impl Universe {
         let high_y = std::cmp::max(c1.y, c2.y);
         (low_x..high_x)
             .filter(|x| self.x_gaps.contains(x))
-            .for_each(|_| dist += 1);
+            .for_each(|_| dist += self.expansion - 1);
         (low_y..high_y)
             .filter(|y| self.y_gaps.contains(y))
-            .for_each(|_| dist += 1);
+            .for_each(|_| dist += self.expansion - 1);
         dist += high_x - low_x;
         dist += high_y - low_y;
         dist
     }
 }
 
-fn compute_shortest_paths(lines: &[String]) -> usize {
+fn compute_shortest_paths(lines: &[String], expansion: usize) -> usize {
     let mut dist = 0;
-    let universe = Universe::new(lines);
+    let universe = Universe::new(lines, expansion);
 
     //println! {"universe: {:?}", universe};
 
@@ -117,17 +120,34 @@ fn compute_shortest_paths(lines: &[String]) -> usize {
 
 #[test]
 fn test_prelim() {
-    let steps = compute_shortest_paths(&get_input("prelim.txt"));
+    let steps = compute_shortest_paths(&get_input("prelim.txt"), 2);
     assert_eq!(steps, 374);
 }
 
 #[test]
 fn test_part1() {
-    let steps = compute_shortest_paths(&get_input("input.txt"));
+    let steps = compute_shortest_paths(&get_input("input.txt"), 2);
     assert_eq!(steps, 10289334);
 }
 
+#[test]
+fn test_prelim2() {
+    let steps = compute_shortest_paths(&get_input("prelim.txt"), 10);
+    assert_eq!(steps, 1030);
+    let steps = compute_shortest_paths(&get_input("prelim.txt"), 100);
+    assert_eq!(steps, 8410);
+}
+
+#[test]
+fn test_part2() {
+    let steps = compute_shortest_paths(&get_input("input.txt"), 1_000_000);
+    assert_eq!(steps, 649862989626);
+}
+
 fn main() {
-    compute_shortest_paths(&get_input("prelim.txt"));
-    compute_shortest_paths(&get_input("input.txt"));
+    compute_shortest_paths(&get_input("prelim.txt"), 2);
+    compute_shortest_paths(&get_input("input.txt"), 2);
+    compute_shortest_paths(&get_input("prelim.txt"), 10);
+    compute_shortest_paths(&get_input("prelim.txt"), 100);
+    compute_shortest_paths(&get_input("input.txt"), 1_000_000);
 }
