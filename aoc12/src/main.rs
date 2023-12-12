c17d472c6b7e81a6546085d8c20279cb93d32f2f
Stephen Hines <srhines@gmail.com>
2023-12-12 03:40:05 -0800

    Use memoization and submit working aoc12 part02.
    The technique ended up a bit ugly, but I really don't think I want to
    spend more time cleaning this up. There's almost certainly a better way
    to be recording the results and returning them.

--- aoc12/src/main.rs
+++ aoc12/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -13,160 +14,355 @@ fn get_input(filename: &str) -> Vec<String> {
     lines
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum SpringState {
     Working, // Valid start state
     InBroken,
     CheckEndBroken,
 }
 
-fn find_matches(springs: &[u8], groups: &[usize], state: SpringState, broken_pos: usize) -> usize {
-    println! {"springs: {}, groups: {:?}, state: {:?}, broken_pos: {}", std::str::from_utf8(springs).unwrap(), groups, state, broken_pos};
-    if springs.is_empty() {
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct Context<'a> {
+    state: SpringState,
+    springs: &'a [u8],
+    broken_pos: usize,
+    groups: &'a [usize],
+}
+
+fn find_matches<'a>(
+    memo: &mut HashMap<(SpringState, usize, usize, usize), usize>,
+    context: Context,
+) -> usize {
+    //println! {"springs: {}, groups: {:?}, state: {:?}, broken_pos: {}", std::str::from_utf8(context.springs).unwrap(), context.groups, context.state, context.broken_pos};
+    let state = (
+        context.state,
+        context.springs.len(),
+        context.broken_pos,
+        context.groups.len(),
+    );
+    if memo.contains_key(&state) {
+        return *memo.get(&state).unwrap();
+    }
+
+    if context.springs.is_empty() {
         // Need to check more about state of broken springs seen
         //println!{"springs.is_empty()"};
-        if state == SpringState::CheckEndBroken || state == SpringState::Working {
+        if context.state == SpringState::CheckEndBroken || context.state == SpringState::Working {
             //println!{"verifying"};
-            if groups.is_empty() {
+            if context.groups.is_empty() {
                 //println!{"groups.is_empty"};
+                memo.insert(state, 1);
                 return 1;
             }
         }
-        println! {"failed"};
+        memo.insert(state, 0);
         return 0;
     }
-    match springs[0] {
+
+    if context.groups.len() != 0 {
+        let remaining_broken: usize = context.groups.iter().sum::<usize>();
+        if remaining_broken - context.broken_pos > context.springs.len() {
+            memo.insert(state, 0);
+            return 0;
+        }
+    }
+
+    match context.springs[0] {
         b'#' => {
-            if groups.is_empty() || state == SpringState::CheckEndBroken {
+            if context.groups.is_empty() || context.state == SpringState::CheckEndBroken {
                 // If we're out of broken springs, we're invalid.
+                memo.insert(state, 0);
                 return 0;
             }
-            if broken_pos + 1 == groups[0] {
+            if context.broken_pos + 1 == context.groups[0] {
                 // Check if we're done on the next iteration.
-                return find_matches(&springs[1..], &groups[1..], SpringState::CheckEndBroken, 0);
+                let new_context = Context {
+                    state: SpringState::CheckEndBroken,
+                    springs: &context.springs[1..],
+                    broken_pos: 0,
+                    groups: &context.groups[1..],
+                };
+                let ret = find_matches(memo, new_context);
+                let state = (
+                    SpringState::CheckEndBroken,
+                    context.springs.len() - 1,
+                    0,
+                    context.groups.len() - 1,
+                );
+                memo.insert(state, ret);
+                return ret;
             }
-            return find_matches(&springs[1..], groups, SpringState::InBroken, broken_pos + 1);
+            let new_context = Context {
+                state: SpringState::InBroken,
+                springs: &context.springs[1..],
+                broken_pos: context.broken_pos + 1,
+                groups: &context.groups,
+            };
+            let ret = find_matches(memo, new_context);
+            let state = (
+                SpringState::InBroken,
+                context.springs.len() - 1,
+                context.broken_pos + 1,
+                context.groups.len(),
+            );
+            memo.insert(state, ret);
+            return ret;
         }
         b'.' => {
-            if state == SpringState::Working {
-                return find_matches(&springs[1..], groups, state, broken_pos);
-            } else if state == SpringState::CheckEndBroken {
-                return find_matches(&springs[1..], groups, SpringState::Working, 0);
-            } else if state == SpringState::InBroken {
+            if context.state == SpringState::Working {
+                let new_context = Context {
+                    state: context.state.clone(),
+                    springs: &context.springs[1..],
+                    broken_pos: 0,
+                    groups: &context.groups,
+                };
+                let ret = find_matches(memo, new_context);
+                let state = (
+                    context.state,
+                    context.springs.len() - 1,
+                    0,
+                    context.groups.len(),
+                );
+                memo.insert(state, ret);
+                return ret;
+            } else if context.state == SpringState::CheckEndBroken {
+                let new_context = Context {
+                    state: SpringState::Working,
+                    springs: &context.springs[1..],
+                    broken_pos: 0,
+                    groups: &context.groups,
+                };
+                let ret = find_matches(memo, new_context);
+                let state = (
+                    SpringState::Working,
+                    context.springs.len() - 1,
+                    0,
+                    context.groups.len(),
+                );
+                memo.insert(state, ret);
+                return ret;
+            } else if context.state == SpringState::InBroken {
                 // We didn't get the right value
+                memo.insert(state, 0);
                 return 0;
             }
         }
         b'?' => {
             // Evaluate multiple options possibly
-            match state {
+            match context.state {
                 SpringState::CheckEndBroken => {
-                    return find_matches(&springs[1..], groups, SpringState::Working, 0);
+                    let new_context = Context {
+                        state: SpringState::Working,
+                        springs: &context.springs[1..],
+                        broken_pos: 0,
+                        groups: &context.groups,
+                    };
+                    let ret = find_matches(memo, new_context);
+                    let state = (
+                        SpringState::Working,
+                        context.springs.len() - 1,
+                        0,
+                        context.groups.len(),
+                    );
+                    memo.insert(state, ret);
+                    return ret;
                 }
                 SpringState::InBroken => {
-                    if broken_pos + 1 == groups[0] {
+                    if context.broken_pos + 1 == context.groups[0] {
                         // Check if we're done on the next iteration.
                         //println!{"Checking ? for InBroken"};
-                        return find_matches(
-                            &springs[1..],
-                            &groups[1..],
+                        let new_context = Context {
+                            state: SpringState::CheckEndBroken,
+                            springs: &context.springs[1..],
+                            broken_pos: 0,
+                            groups: &context.groups[1..],
+                        };
+                        let ret = find_matches(memo, new_context);
+                        let state = (
                             SpringState::CheckEndBroken,
+                            context.springs.len() - 1,
                             0,
+                            context.groups.len() - 1,
                         );
+                        memo.insert(state, ret);
+                        return ret;
                     }
                     //println!{"Checking continued for InBroken: {} {}", broken_pos, groups[0]};
-                    return find_matches(
-                        &springs[1..],
-                        groups,
+                    let new_context = Context {
+                        state: SpringState::InBroken,
+                        springs: &context.springs[1..],
+                        broken_pos: context.broken_pos + 1,
+                        groups: &context.groups,
+                    };
+                    let ret = find_matches(memo, new_context);
+                    let state = (
                         SpringState::InBroken,
-                        broken_pos + 1,
+                        context.springs.len() - 1,
+                        context.broken_pos + 1,
+                        context.groups.len(),
                     );
+                    memo.insert(state, ret);
+                    return ret;
                 }
                 SpringState::Working => {
                     let mut total = 0;
-                    if !groups.is_empty() {
-                        assert_eq!(broken_pos, 0);
-                        if groups[0] == 1 {
+                    if !context.groups.is_empty() {
+                        assert_eq!(context.broken_pos, 0);
+                        if context.groups[0] == 1 {
                             // Handle this case without hassle
-                            total += find_matches(
-                                &springs[1..],
-                                &groups[1..],
+                            let new_context = Context {
+                                state: SpringState::CheckEndBroken,
+                                springs: &context.springs[1..],
+                                broken_pos: 0,
+                                groups: &context.groups[1..],
+                            };
+                            let ret = find_matches(memo, new_context);
+                            let state = (
                                 SpringState::CheckEndBroken,
+                                context.springs.len() - 1,
                                 0,
+                                context.groups.len() - 1,
                             );
+                            memo.insert(state, ret);
+                            total += ret;
                         } else {
-                            total += find_matches(
-                                &springs[1..],
-                                groups,
+                            let new_context = Context {
+                                state: SpringState::InBroken,
+                                springs: &context.springs[1..],
+                                broken_pos: context.broken_pos + 1,
+                                groups: &context.groups,
+                            };
+                            let ret = find_matches(memo, new_context);
+                            let state = (
                                 SpringState::InBroken,
-                                broken_pos + 1,
+                                context.springs.len() - 1,
+                                context.broken_pos + 1,
+                                context.groups.len(),
                             );
+                            memo.insert(state, ret);
+                            total += ret;
                         }
                     }
-                    return total + find_matches(&springs[1..], groups, state, 0);
+                    let new_context = Context {
+                        state: SpringState::Working,
+                        springs: &context.springs[1..],
+                        broken_pos: 0,
+                        groups: &context.groups,
+                    };
+                    let ret = find_matches(memo, new_context);
+                    let state = (
+                        SpringState::Working,
+                        context.springs.len() - 1,
+                        0,
+                        context.groups.len(),
+                    );
+                    memo.insert(state, ret);
+                    return ret + total;
                 }
             }
         }
         _ => {
-            panic!("Unknown input {:?}", springs);
+            panic!("Unknown input {:?}", &context.springs);
         }
     }
     0
 }
 
-fn get_arrangements(line: &str) -> usize {
+fn get_arrangements(line: &str, damaged: bool) -> usize {
     let toks: Vec<_> = line.split_whitespace().collect();
-    let groups: Vec<_> = toks[1]
+    let mut expanded_springs_str = String::new();
+    let mut expanded_groups_str = String::new();
+    let groups_tok = if !damaged {
+        toks[1]
+    } else {
+        for _ in 0..4 {
+            expanded_groups_str += toks[1];
+            expanded_groups_str += ",";
+        }
+        expanded_groups_str += toks[1];
+        //println! {"expanded_groups_str: {}", expanded_groups_str};
+        expanded_groups_str.as_str()
+    };
+    let groups: Vec<_> = groups_tok
         .split(",")
         .map(|g| g.parse::<usize>().unwrap())
         .collect();
-    let springs = toks[0];
-    println! {"groups: {:?}", groups};
-    //println!{"springs: {:?}", springs};
+    let springs = if !damaged {
+        toks[0]
+    } else {
+        for _ in 0..4 {
+            expanded_springs_str += toks[0];
+            expanded_springs_str += "?";
+        }
+        expanded_springs_str += toks[0];
+        //println! {"expanded_springs_str: {}", expanded_springs_str};
+        expanded_springs_str.as_str()
+    };
 
     let mut arr = 0;
-    /*if valid(springs, &groups) {
-        arr += 1;
-    }*/
 
-    arr += find_matches(
-        springs.as_bytes(),
-        groups.as_slice(),
-        SpringState::Working,
-        0,
-    );
-    println! {"arrangements {}: {}", line, arr};
+    let mut memo: HashMap<_, _> = HashMap::new();
+
+    let new_context = Context {
+        state: SpringState::Working,
+        springs: springs.as_bytes(),
+        broken_pos: 0,
+        groups: groups.as_slice(),
+    };
+    arr += find_matches(&mut memo, new_context);
+    //println! {"arrangements {}: {}", line, arr};
 
     arr
 }
 
-fn get_total_arrangements(lines: &[String]) -> usize {
-    let sum = lines.iter().map(|l| get_arrangements(l)).sum();
+fn get_total_arrangements(lines: &[String], damaged: bool) -> usize {
+    let sum = lines.iter().map(|l| get_arrangements(l, damaged)).sum();
     println! {"total arrangements: {}", sum};
     sum
 }
 
+#[test]
 fn test_basic() {
     let line = "#.#.### 1,1,3";
-    get_arrangements(line);
+    assert_eq!(get_arrangements(line, false), 1);
     let line = "???.### 1,1,3";
-    get_arrangements(line);
+    assert_eq!(get_arrangements(line, false), 1);
 }
 
 #[test]
 fn test_prelim_full() {
-    let arr = get_total_arrangements(&get_input("prelim_full.txt"));
+    let arr = get_total_arrangements(&get_input("prelim_full.txt"), false);
     assert_eq!(arr, 6);
 }
 
 #[test]
 fn test_prelim() {
-    let arr = get_total_arrangements(&get_input("prelim.txt"));
+    let arr = get_total_arrangements(&get_input("prelim.txt"), false);
     assert_eq!(arr, 21);
 }
 
+#[test]
+fn test_part1() {
+    let arr = get_total_arrangements(&get_input("input.txt"), false);
+    assert_eq!(arr, 7718);
+}
+
+#[test]
+fn test_prelim2() {
+    let arr = get_total_arrangements(&get_input("prelim.txt"), true);
+    assert_eq!(arr, 525152);
+}
+
+#[test]
+fn test_part2() {
+    let arr = get_total_arrangements(&get_input("input.txt"), true);
+    assert_eq!(arr, 128741994134728);
+}
+
 fn main() {
-    get_total_arrangements(&get_input("prelim_full.txt"));
-    get_total_arrangements(&get_input("prelim.txt"));
-    get_total_arrangements(&get_input("input.txt"));
+    get_total_arrangements(&get_input("prelim_full.txt"), false);
+    get_total_arrangements(&get_input("prelim.txt"), false);
+    get_total_arrangements(&get_input("input.txt"), false);
+    get_total_arrangements(&get_input("prelim.txt"), true);
+    get_total_arrangements(&get_input("input.txt"), true);
 }
